af8af7d8c71c357a2c53823d5ab5eb786c77090d
logan <92327020+DevLoggith@users.noreply.github.com>
2025-08-25 12:04:48 -0400

    create root, public, and json routes
    also integrate .env configs

--- myApp.js
+++ myApp.js
@@ -1,39 +1,28 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+require("dotenv").config();
 
+app.get("/", (req, res) => {
+	absolutePath = __dirname + "/views/index.html";
+	res.sendFile(absolutePath);
+});
 
+app.use("/public", express.static(__dirname + "/public"));
 
+app.use((req, res, next) => {
+	console.log(`${req.method} ${req.path} - ${req.ip}`);
+	next();
+});
 
+app.get("/json", (req, res) => {
+	let message = { message: "Hello json" };
 
+	if (process.env.MESSAGE_STYLE === "uppercase") {
+		message.message = message.message.toUpperCase();
+	}
 
+	res.json(message);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
